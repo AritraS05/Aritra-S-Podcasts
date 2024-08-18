3f4dc2ef13af907b9b5acf248770d4743dc4b1ef
Aritra Sarkar <aritrasarkar00007@gmail.com>
2024-08-18 12:49:33 +0530

    http protocol user schema and auth merger route dn

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://awake-hog-78.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
